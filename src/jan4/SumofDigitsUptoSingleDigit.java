cc11c203d848eb003015cfa259ff5e957582cad6
SANJAY V <sanjay3005v@gmail.com>
2025-01-07 18:02:37 +0530

    Commit 8

---
+++ src/jan4/SumofDigitsUptoSingleDigit.java
@@ -0,0 +1,26 @@
+package jan4;
+import java.util.Scanner;
+public class SumofDigitsUptoSingleDigit {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = scan.nextInt();
+		Sumdigits(num);
+	}
+	public static void Sumdigits(int num) {
+		int sum=0;
+		while(num>0) {
+			int rem=num%10;
+			sum+=rem;
+			num/=10;
+		}
+		if(sum>9) {
+			Sumdigits(sum);
+		}
+		else {
+			System.out.println("The sum of the digits is "+sum);
+			return;
+		}
+	}
+	
+}
