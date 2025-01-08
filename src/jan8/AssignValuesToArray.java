1b6530a2b247ecf73969466979d6f8f948c102b9
SANJAY V <sanjay3005v@gmail.com>
2025-01-08 15:29:46 +0530

    Jan8

---
+++ src/jan8/AssignValuesToArray.java
@@ -0,0 +1,23 @@
+package jan8;
+
+public class AssignValuesToArray {
+	public static void main(String[] args) {
+		int a[]=new int[4];
+		System.out.println(a);
+		System.out.println(a.length);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		System.out.println(a);
+		System.out.println(a.length);
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+	}
+}
---
+++ src/jan8/InputArray.java
@@ -0,0 +1,17 @@
+package jan8;
+import java.util.Scanner;
+public class InputArray {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int size = scan.nextInt();
+		System.out.println("Enter the elements");
+		int[] a = new int[size];
+		for(int i=0;i<size;i++) {
+			a[i]=scan.nextInt();
+		}
+		for(int i=0;i<size;i++) {
+			System.out.println(a[i]);
+		}
+	}
+}
