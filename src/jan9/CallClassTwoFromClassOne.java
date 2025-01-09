b84c0f370be13e3322e391152f268d0a1895cc4b
SANJAY V <sanjay3005v@gmail.com>
2025-01-09 12:26:57 +0530

    Jan9

---
+++ src/jan9/CallClassTwoFromClassOne.java
@@ -0,0 +1,9 @@
+package jan9;
+
+public class CallClassTwoFromClassOne {
+	public static void main(String[] args) {
+		String[] a=null;
+		class2.main(a);
+	}
+}
+
---
+++ src/jan9/class2.java
@@ -0,0 +1,8 @@
+package jan9;
+
+public class class2 {
+	public static void main(String[] args) {
+		System.out.println(args);
+		System.out.println("Class2");
+	}
+}
