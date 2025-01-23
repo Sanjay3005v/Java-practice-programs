2fcbe811f09281febfb7ad5007f83daf0a7a925f
SANJAY V <sanjay3005v@gmail.com>
2025-01-23 09:17:33 +0530

    Jan23

--- src/jan4/rough.java
+++ src/jan4/rough.java
@@ -2,16 +2,17 @@ package jan4;
 
 public class rough {
 	public static void main(String[] args) {
-		for(int row=1;row<=5;row++) {
-			for(int col=1;col<=5;col++) {
-				if(row+col>=6) {
-					System.out.print(" * ");
+			int num=5;
+			for(int row=1;row<=num;row++) {
+				for(int col=1;col<=2*num-1;col++) {
+					if((col+row>num && col-row<=num-1)) {
+						System.out.print(" * ");
+					}
+					else {
+						System.out.print("  ");
+					}
 				}
-				else {
-					System.out.print("   ");
-				}
+				System.out.println();
 			}
-			System.out.println();
 		}
-	}
 }
