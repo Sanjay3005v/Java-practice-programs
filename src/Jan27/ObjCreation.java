4cb1f60a395766f4f3552c7893fa61129db91c54
SANJAY V <sanjay3005v@gmail.com>
2025-01-27 13:23:37 +0530

    Jan27

---
+++ src/Jan27/ObjCreation.java
@@ -0,0 +1,20 @@
+package Jan27;
+
+public class ObjCreation {
+	String  brand;
+	double price;
+	String ram;
+	public static void display(ObjCreation Mobile) {
+		
+		System.out.println(Mobile.brand);
+		
+	}
+	
+	public static void main(String[] args) {
+		ObjCreation Mobile= new ObjCreation();
+		System.out.println(Mobile.brand);
+		System.out.println(Mobile.price);
+		System.out.println(Mobile.ram);
+		Mobile.display(Mobile);
+	}
+}
--- src/jan4/rough.java
+++ src/jan4/rough.java
@@ -1,28 +1,16 @@
 package jan4;
 public class rough {
-    public static void moveNegativesToLeft(int[] arr) {
-        int pos = 0; // Pointer for the position to place the next negative number
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) {
-                int temp = arr[i];
-                // Shift elements to maintain order
-                for (int j = i; j > pos; j--) {
-                    arr[j] = arr[j - 1];
-                }
-                arr[pos] = temp;
-                pos++;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {3, -2, 4, -1, 0, -5, 8};
-        moveNegativesToLeft(arr);
-
-        // Print rearranged array
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
-}
+	
+	    public int removeDuplicates(int[] arr) {
+	        int i=0;
+	        for(int j=1;j<arr.length;j++){
+	            if(arr[i]!=arr[j]){
+	                i++;
+	                arr[i]=arr[j];
+	            }
+	        }
+	        return i+1;
+	        
+	    }
+	
+	}
