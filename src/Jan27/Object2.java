a8b08c0cd88255ed691b1b6ff47acf0b6aabd9b1
SANJAY V <sanjay3005v@gmail.com>
2025-01-28 12:25:21 +0530

    Jan28

--- src/Jan27/Object2.java
+++ src/Jan27/Object2.java
@@ -1,20 +1,25 @@
-//Design a class for an object and initialize the state of the object using Non-static method and display the states 
+//Design a class for an object and initialize the state of the object using Non-static method and display the states  
 //using non static method-->2 separate method to initialize and to display.
 
 
 package Jan27;
 public class Object2 {
-	public static void main(String[] args) {
-		Car car1 = new Car("Chevrolet","Corvette",7000000);		
-		car1.display();
-	}
-	
-}
-class Car{
 	String Company;
 	String Model;
 	long price;
-	public Car(String Company,String Model,long price) {
+	public static void main(String[] args) {
+		Cars car1 = new Cars("Chevrolet","Corvette",7000000);	
+		Cars car2 = new Cars("Porche","911",3000000);
+		car1.display();
+		car2.display();
+	}
+}
+class Cars{
+	String Company;
+	String Model;
+	long price;
+	
+	public  Cars(String Company,String Model,long price) {
 		this.Company=Company;
 		this.Model=Model;
 		this.price=price;
